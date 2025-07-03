b3a8a7948e127a9e0f3c78b284b33cbae3551f6a
DottsGit <6w1598yft5kp@maskedmails.com>
2025-07-03 16:37:38 -0400

    Stylized the buttons at the bottom

--- src/components/BottomNavBar.vue
+++ src/components/BottomNavBar.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="fixed bottom-0 left-0 right-0 h-16 bg-black z-40 flex justify-center items-center border-t border-gray-700">
     <div class="flex w-full">
-      <router-link to="/" class="flex-1 text-center py-2 text-lg text-white hover:text-pink-500">
-        Feed
+      <router-link to="/" class="flex-1 flex justify-end items-center pr-10 text-lg text-white hover:text-pink-500 border-r border-gray-700">
+        <span>Feed</span>
       </router-link>
-      <div class="w-px h-full bg-gray-700"></div>
-      <router-link to="/profile" class="flex-1 text-center py-2 text-lg text-white hover:text-pink-500">
-        Profile
+      <router-link to="/profile" class="flex-1 flex justify-start items-center pl-10 text-lg text-white hover:text-pink-500">
+        <span>Profile</span>
       </router-link>
     </div>
   </div>
